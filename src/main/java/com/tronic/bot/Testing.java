d41e5b2ed4cc8cf92ce1655b1aecf7aed1a3a0c7
Florian Weichert <florian.weichert@unitybox.de>
2019-11-21 20:06:02 +0100

    Delete Testing

--- src/main/java/com/tronic/bot/Testing.java
+++
@@ -1,9 +0,0 @@
-package com.tronic.bot;
-
-public class Testing {
-
-    public static void main(String[] args) throws Exception {
-        new Tronic("NTU0ODAzODg0NDM5MjQwNzA1.XdW7dw.dkuz8cSA29WAGblDvVxix3WeUHs");
-    }
-
-}
