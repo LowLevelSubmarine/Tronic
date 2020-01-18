bb79c3d9f202b87ad9f51df5cccbd50651ba2c75
Jonbeckas <Jonasbeckerx3@outlook.de>
2020-01-19 00:23:40 +0100

    bugfix in initial json

--- src/main/java/com/tronic/updater/Updater.java
+++ src/main/java/com/tronic/updater/Updater.java
@@ -51,7 +51,7 @@ public class Updater {
         String ptJar = JavaTools.getJarUrl(Updater.class);
         try {
             FileWriter fw = new FileWriter(ptJar+"/tronic.json");
-            fw.write("[{\"procname\": \"java -jar "+getCurrentJARFilePath().toString()+".jar\", \"if\":\"\",\"else\":\"java -jar "+getCurrentJARFilePath().toString()+".jar\" }]");
+            fw.write("[{\"procname\": \"java -jar "+getCurrentJARFilePath().toString()+".jar\", \"if\":\"\",\"else\":\"java -jar "+getCurrentJARFilePath().toString()+"\" }]");
             fw.close();
         } catch (URISyntaxException e) {
             e.printStackTrace();
