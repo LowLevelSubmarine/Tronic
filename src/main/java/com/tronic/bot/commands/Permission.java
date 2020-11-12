10fc71d07b16a03f3b439c91ff251c524771ff20
Jonas Becker <jonasbeckerx3@outlook.de>
2020-11-12 22:53:04 +0100

    bugfix Permission.java

--- src/main/java/com/tronic/bot/commands/Permission.java
+++ src/main/java/com/tronic/bot/commands/Permission.java
@@ -3,9 +3,12 @@ package com.tronic.bot.commands;
 import com.tronic.bot.core.Core;
 import com.tronic.bot.core.Tronic;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.Event;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.Objects;
 
 public enum Permission {
 
@@ -21,7 +24,8 @@ public enum Permission {
     public boolean isValid (MessageReceivedEvent event, Core tronic) {
         boolean isCoHost =tronic.getStorage().getStatic().isCoHoster(event.getAuthor());
         boolean isHost = event.getAuthor().equals(tronic.getJDA().getUserById(tronic.getHostToken()));
-        boolean isAdmin = event.getGuild().getMember(event.getAuthor()).hasPermission(net.dv8tion.jda.api.Permission.ADMINISTRATOR);
+        Member author = event.getGuild().getMember(event.getAuthor());
+        boolean isAdmin = author != null && author.hasPermission(net.dv8tion.jda.api.Permission.ADMINISTRATOR);
         if (this.level == HOST.level) {
             return isHost;
         } else if (this.level == CO_HOST.level) {
@@ -38,7 +42,8 @@ public enum Permission {
     public static Permission getUserPermission(User user,Guild guild, Core core) {
         boolean isCoHost =core.getStorage().getStatic().isCoHoster(user);
         boolean isHost = user.equals(core.getJDA().getUserById(core.getHostToken()));
-        boolean isAdmin = guild.getMember(user).hasPermission(net.dv8tion.jda.api.Permission.ADMINISTRATOR);
+        Member author = guild.getMember(user);
+        boolean isAdmin = author != null && author.hasPermission(net.dv8tion.jda.api.Permission.ADMINISTRATOR);
         if (isHost) {
             return HOST;
         } else if (isCoHost) {
