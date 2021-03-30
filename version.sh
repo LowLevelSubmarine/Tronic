56b9996d3a428aa4bd691dc0b3364a294d0e6a0c
Jonbeckas <32589685+Jonbeckas@users.noreply.github.com>
2021-03-30 22:28:04 +0200

    Ci (#93)
    Add some CI Automatisation to create release on start

--- version.sh
+++ version.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # shellcheck disable=SC2002
-TMP=$(cat build.gradle | grep -m2 -Po '(?<=(version)).*($)' | tail -n1 | sed -r "s/(')//g");
-major=$(echo $TMP | cut -d. -f1);
-minor=$(echo $TMP | cut -d. -f2);
-patch=$(echo $TMP | cut -d. -f3);
-OLD=$major\.$minor\.$patch
-patch=$((patch+1));
-NEW=$major\.$minor\.$patch;
-sed -i -- "s/$OLD/$NEW/g" build.gradle;
-echo $NEW;
+TMP=$(cat build.gradle | grep -m2 -Po '(?<=(version)).*($)' | tail -n1);
+#major=$(echo $TMP | cut -d. -f1);
+#minor=$(echo $TMP | cut -d. -f2);
+#patch=$(echo $TMP | cut -d. -f3);
+#OLD=$major\.$minor\.$patch
+#patch=$((patch+1));
+#NEW=$major\.$minor\.$patch;
+sed -i -- "s/$TMP/ '$1'/g" build.gradle;
+echo $1;
+
+# NEVER GONNA GIVE YOU AUP
