e8de37850c1cdf4203b7dfff601f2be4dcbf400e
LowLevelSubmarine <florian.weichert@unitybox.de>
2021-12-04 18:51:47 +0100

    Improved Loggy readability

--- src/main/java/com/tronic/logger/Event.kt
+++ src/main/java/com/tronic/logger/Event.kt
@@ -1,12 +1,16 @@
 package com.tronic.logger
 
+import com.lowlevelsubmarine.subsconsole.Align
 import java.io.PrintWriter
 import java.io.StringWriter
 
 data class Event(val clazz: Class<*>, val level: Level, val string: String?, val exception: Exception?) {
 
     override fun toString(): String {
-        val stamp = "[${this.level.name.tint(this.level.color)} ${Loggy.timestamp()} ${Loggy.shortClassName(this.clazz)}]"
+        val timestamp = Loggy.timestamp()
+        val level = Align.LEFT.align(this.level.name.tint(this.level.color), 5)
+        val location = Align.LEFT.align(Loggy.shortClassName(this.clazz), 25)
+        val stamp = "[$level $timestamp $location]"
         if (this.exception == null) {
             return "$stamp $string"
         } else {
@@ -18,7 +22,7 @@ data class Event(val clazz: Class<*>, val level: Level, val string: String?, val
             }
         }
     }
-    
+
     fun Exception.toPrintString(): String {
         val errorString = StringWriter()
         this.printStackTrace(PrintWriter(errorString))
