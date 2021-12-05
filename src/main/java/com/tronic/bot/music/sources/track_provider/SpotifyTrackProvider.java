46e76ec9309a78c166f7ee4000a667c207f3defb
LowLevelSubmarine <florian.weichert@unitybox.de>
2021-12-05 22:42:33 +0100

    feat: simpler approach to ensuring an up-to-date access token

--- src/main/java/com/tronic/bot/music/sources/track_provider/SpotifyTrackProvider.java
+++ src/main/java/com/tronic/bot/music/sources/track_provider/SpotifyTrackProvider.java
@@ -71,7 +71,7 @@ public class SpotifyTrackProvider implements UrlTrackProvider {
 
     private QueueItem queueItemFromTrackId(String id) {
         try {
-            return queueItemFromSpotifyTrack(executeRequest(() -> api.getTrack(id)));
+            return queueItemFromSpotifyTrack(executeRequest(api.getTrack(id)));
         } catch (Exception e) {
             Loggy.logD("Failed creating queue item from spotify track id " + id + " " + e);
             return null;
@@ -80,14 +80,13 @@ public class SpotifyTrackProvider implements UrlTrackProvider {
 
     private QueueItem queueItemFromPlaylistId(String id, int maxTracks) {
         try {
-            Playlist playlist = executeRequest(() -> this.api.getPlaylist(id));
-            Paging<PlaylistTrack> paging = executeRequest(() -> this.api.getPlaylistsItems(id).limit(100));
+            Playlist playlist = executeRequest(this.api.getPlaylist(id));
+            Paging<PlaylistTrack> paging = executeRequest(this.api.getPlaylistsItems(id).limit(100));
             List<PlaylistTrack> playlistTracks = new LinkedList<>();
             while (paging != null) {
                 playlistTracks.addAll(Arrays.asList(paging.getItems()));
                 if (paging.getNext() != null) {
-                    final int offset = paging.getOffset() + 100;
-                    paging = executeRequest(() -> this.api.getPlaylistsItems(id).limit(100).offset(offset));
+                    paging = executeRequest(this.api.getPlaylistsItems(id).limit(100).offset(paging.getOffset() + 100));
                 } else {
                     paging = null;
                 }
@@ -108,18 +107,37 @@ public class SpotifyTrackProvider implements UrlTrackProvider {
         }
     }
 
-    private <T> T executeRequest(RequestBlueprint<T> blueprint) throws IOException, SpotifyWebApiException, ParseException {
+    /**
+     * Executes the given request while dealing with invalid token responses.
+     * If executing the request fails due to an invalid access token,
+     * the access token updates and the request executes for a second try.
+     * @param builder The builder of the request
+     * @param <T> The type of the builders response
+     * @return The builders response
+     * @throws IOException Is thrown when the reques fails
+     * @throws SpotifyWebApiException Is thrown when the request fails
+     * @throws ParseException Is thrown when the request fails
+     */
+    private synchronized <T> T executeRequest(AbstractDataRequest.Builder<T, ?> builder) throws IOException, SpotifyWebApiException, ParseException {
         try {
-            return blueprint.constructRequest().build().execute();
+            return builder.build().execute();
         } catch (UnauthorizedException e) {
             Loggy.logD(e + " Executing access token refresh ...");
             updateApiToken();
-            return blueprint.constructRequest().build().execute();
+            forceUpdateAccessToken(builder);
+            return builder.build().execute();
         }
     }
-    
-    private interface RequestBlueprint<T> {
-        AbstractDataRequest.Builder<T, ?> constructRequest();
+
+    /**
+     * Overwrites the access token of a request builder with the apis current one.
+     * This is needed, as the library writes the access token to the builder
+     * when it is beeing constructed, which makes it impossibile to resend
+     * after the access token has been updated.
+     * @param builder The builder to overwrite the access token of
+     */
+    private void forceUpdateAccessToken(AbstractDataRequest.Builder<?, ?> builder) {
+        builder.setHeader("Authorization", "Bearer " + this.api.getAccessToken());
     }
 
     private void updateApiToken() throws IOException, SpotifyWebApiException, ParseException {
